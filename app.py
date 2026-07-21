b26a6c84dc25ef96265158d62a9068761b77665a
Ali Baker <alisondre.baker@gmail.com>
2026-07-21 17:07:38 -0400

    Added User Endpoints

--- app.py
+++ app.py
@@ -89,3 +89,64 @@ products_schema = ProductSchema(many=True)
 #===========================
 # IMPLEMENT CRUD ENDPOINTS
 #===========================
+
+# ----USER ENDPOINTS--------
+# Create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    new_user = User(name=user_data['name'], address=user_data['address'], email=user_data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return user_schema.jsonify(new_user), 201
+
+# Retrieve All Users
+@app.route('/users', methods=['GET'])
+def get_users():
+    query = select(User)
+    users = db.session.execute(query).scalars().all()
+    
+    return users_schema.jsonify(users), 200
+
+# Retrieve a SINGLE User by ID
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = db.session.get(User, id)
+    return user_schema.jsonify(user), 200
+
+# Update User
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = db.session.get(User, id) 
+    
+    if not user:
+        return jsonify({"message": 'Invalid user id'}), 400
+    
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    user.name = user_data['name']
+    user.address = user_data['address']
+    user.email = user_data['email']
+    
+    db.session.commit()
+    return user_schema.jsonify(user), 200
+
+# Delete a User
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = db.session.get(User, id)
+    
+    if not user:
+        return jsonify({'message': 'Invalid user id'}), 400
+    
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': f'Successfully deleted user {id}'}), 200
